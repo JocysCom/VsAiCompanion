6dc8a24628546b91c6ef95142b9f6ef2cd3e13ae
Vaidotas Jocys <vaidotas@jocys.com>
2023-08-14 01:50:06 +0100

    Theme update.

--- Engine/JocysCom/Configuration/SettingsData.cs
+++ Engine/JocysCom/Configuration/SettingsData.cs
@@ -10,7 +10,7 @@ using System.Runtime.Serialization;
 using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
-using System.Xml.Linq;
+//using System.Xml.Linq;
 using JocysCom.ClassLibrary.Controls;
 #if NETSTANDARD // .NET Standard
 #elif NETCOREAPP // .NET Core
