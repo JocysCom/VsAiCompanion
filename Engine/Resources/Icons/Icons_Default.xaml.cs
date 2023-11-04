02cced60c87fbf983c97106984e59f29e9a53a78
Evaldas Jocys <evaldas@jocys.com>
2023-11-04 22:00:05 +0000

    Update icons.

--- Engine/Resources/Icons/Icons_Default.xaml.cs
+++ Engine/Resources/Icons/Icons_Default.xaml.cs
@@ -27,6 +27,7 @@ namespace JocysCom.VS.AiCompanion.Engine.Resources.Icons
 		public const string Icon_clipboard_checks = nameof(Icon_clipboard_checks);
 		public const string Icon_clipboard_check_edit = nameof(Icon_clipboard_check_edit);
 		public const string Icon_clipboard_paste = nameof(Icon_clipboard_paste);
+		public const string Icon_cloud = nameof(Icon_cloud);
 		public const string Icon_cloud_download = nameof(Icon_cloud_download);
 		public const string Icon_cloud_folder = nameof(Icon_cloud_folder);
 		public const string Icon_cloud_gear = nameof(Icon_cloud_gear);
