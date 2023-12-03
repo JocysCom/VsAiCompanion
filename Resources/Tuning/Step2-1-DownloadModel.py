60805ea50a2c97ce33dddeedeb7a5ffeed214456
Evaldas Jocys <evaldas@jocys.com>
2023-12-03 21:14:37 +0000

    Updating to use LLaMA-2

--- Resources/Tuning/Step2-1-DownloadModel.py
+++ Resources/Tuning/Step2-1-DownloadModel.py
@@ -1,7 +1,7 @@
 # This script is responsible for downloading the pre-trained model and its tokenizer using the transformers library by Hugging Face. It defines a function download_model that takes a model name and a path to save the model and tokenizer. It controls the handling of certificate files for HTTP requests and sets environment variables accordingly. The script finally downloads and saves the GPT-2 model and tokenizer to a specified path on the local file system.
 
 import os
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Path to the .pem file that contains the trusted root certificates
 CERT_FILE_PATH = './Data/trusted_root_certificates.pem'
@@ -12,19 +12,24 @@ if os.path.exists(CERT_FILE_PATH) and os.path.getsize(CERT_FILE_PATH) > 0:
 
 os.environ['HF_HUB_DISABLE_SYMLINKS_WARNING'] = '1'
 
-def download_model(model_name, model_path):
-    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-    model = GPT2LMHeadModel.from_pretrained(model_name)
+def download_and_cache_model(model_name, cache_dir="./model_cache"):
+    """
+    Download and cache a model and its tokenizer using the transformers library.
     
-    # Save the downloaded model & tokenizer in the specified model path
-    tokenizer.save_pretrained(model_path)
-    model.save_pretrained(model_path)
+    :param model_name: identifier of the model on Hugging Face Model Hub (string)
+    :param cache_dir: directory where the model and tokenizer will be cached (string)
+    """
+    # Download and cache the model
+    model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir=cache_dir)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
+    return model, tokenizer
 
-if __name__ == '__main__':
-    # Define the model name and the path where the model is to be saved
-    MODEL_NAME = 'gpt2'  # 'gpt2' is a smaller version of the model
-    MODEL_SAVE_PATH = './Models/OpenAI/GPT2/'
+# Example usage
+if __name__ == "__main__":
+    model_name = "microsoft/Orca-2-7b"  # The name of the model on Hugging Face Model Hub
+    cache_dir = "./llama2_model_cache"  # Customize this path as necessary
     
-    # Download the model and tokenizer
-    download_model(MODEL_NAME, MODEL_SAVE_PATH)
-    print(f"The {MODEL_NAME} model and tokenizer have been downloaded and saved to {MODEL_SAVE_PATH}")
+    # Download and cache the model and tokenizer
+    model, tokenizer = download_and_cache_model(model_name, cache_dir)
+    
+    print(f"{model_name} has been downloaded and cached.")
