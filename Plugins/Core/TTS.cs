7f3ad2921413c9d4e67195d578ba7b5a792fd276
Vaidotas Jocys <vaidotas@jocys.com>
2024-03-04 20:14:44 +0000

    TTS.cs summary updated

--- Plugins/Core/TTS.cs
+++ Plugins/Core/TTS.cs
@@ -10,10 +10,10 @@ namespace JocysCom.VS.AiCompanion.Plugins.Core
 	/// <summary>
 	/// TTS functions allow AI to speak, voice, and narrate stories and dialogues. They are used with Jocys.com TTS Monitor portable application from Jocys.com/TTS. Works well in combination with Windows Voice Typing. 
 	///
-	/// How to enable text-to-speech feature in AI Companion: 
+	/// Enable text-to-speech in AI Companion: 
 	/// Enable all TTS plugins in this tab. Add message instruction for AI like "Answer with text-to-speech voice." 
 	///
-	/// How to enable text-to-speech feature for AI Companion in TTS Monitor: 
+	/// Enable text-to-speech for AI Companion in TTS Monitor: 
 	/// Open[Options] tab > open[Monitor: Server] tab > check "Enable" checkbox. 
 	/// Open[Options] tab > open[Monitor: Display] tab > uncheck "Enable" checkbox.
 	/// </summary>
