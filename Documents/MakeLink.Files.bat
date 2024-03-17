9807d9c9a24b231ef84891b574b68e28d933cbde
Evaldas Jocys <evaldas@jocys.com>
2024-03-17 20:17:01 +0000

    Update templates.

---
+++ Documents/MakeLink.Files.bat
@@ -0,0 +1,2 @@
+mklink /D "d:\Projects\Jocys.com GitHub\VsAiCompanion\Documents\Files\AiCompanion" "d:\Projects\Jocys.com GitHub\VsAiCompanion\Engine\Resources\Settings.CompanyName"
+mklink /D "d:\Projects\Jocys.com GitHub\VsAiCompanion\Documents\Files\JocysCom.VS.AiCompanion.App" "d:\Projects\Jocys.com GitHub\VsAiCompanion\Engine\Resources\Settings"
