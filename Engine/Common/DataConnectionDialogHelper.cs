11d9a804d3dbc9eabd2d9f63c9c0845bd6d9bf63
Evaldas Jocys <evaldas@jocys.com>
2024-04-07 21:57:55 +0100

    SQL dialogs.

--- Engine/Common/DataConnectionDialogHelper.cs
+++ Engine/Common/DataConnectionDialogHelper.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-#if NETFRAMEWORK
 using Microsoft.Data.ConnectionUI;
+#if NETFRAMEWORK
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure.DependencyResolution;
 using System.Data.SqlClient;
@@ -9,7 +9,7 @@ using System.Data.SQLite.EF6;
 using Microsoft.Data.SqlClient;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.SqlServer.Management.ConnectionUI;
+//using Microsoft.SqlServer.Management.ConnectionUI;
 #endif
 
 namespace JocysCom.VS.AiCompanion.Engine
@@ -20,6 +20,7 @@ namespace JocysCom.VS.AiCompanion.Engine
 		{
 			var dcd = new DataConnectionDialog();
 			dcd.DataSources.Clear();
+			DataSource.AddStandardDataSources(dcd);
 			dcd.DataSources.Add(DataSource.SqlDataSource);
 			dcd.SelectedDataProvider = DataProvider.SqlDataProvider;
 			dcd.SelectedDataSource = DataSource.SqlDataSource;
@@ -51,6 +52,42 @@ namespace JocysCom.VS.AiCompanion.Engine
 			str = str.Replace("Trust Server Certificate", "TrustServerCertificate");
 			return str;
 		}
+
+		/*
+		public void OpenDatabaseConnectionDialog()
+		{
+			string filePath = Path.GetTempFileName() + ".udl";
+			Process.Start(filePath);
+		}
+
+		public void MonitorFileChange()
+		{
+			string filePath = Path.GetTempFileName() + ".udl";
+			using (var watcher = new FileSystemWatcher(Path.GetDirectoryName(filePath), "*.udl"))
+			{
+				watcher.NotifyFilter = NotifyFilters.LastWrite;
+				watcher.Changed += (object sender, FileSystemEventArgs e) =>
+			{
+					if (e.FullPath == filePath)
+					{
+						string[] lines = File.ReadAllLines(filePath);
+						foreach (string line in lines)
+						{
+							if (line.StartsWith("Provider="))
+							{
+								string connectionString = line.Substring("Provider=".Length);
+								// use the connection string
+								Debug.WriteLine(connectionString);
+							}
+						}
+					}
+				};
+				watcher.EnableRaisingEvents = true;
+				OpenDatabaseConnectionDialog(filePath);
+				Console.ReadLine();
+			}
+		}
+		*/
 
 	}
 }
