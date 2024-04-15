24857adb655f65333caffab5d9857920cabd0356
Evaldas Jocys <evaldas@jocys.com>
2024-04-15 09:19:19 +0100

    Fix crash.

--- Engine/Common/TaskSettings.cs
+++ Engine/Common/TaskSettings.cs
@@ -36,7 +36,7 @@ namespace JocysCom.VS.AiCompanion.Engine
 		public List<string> ListSelection { get => _ListSelection; set => SetProperty(ref _ListSelection, value); }
 		private List<string> _ListSelection;
 
-		public List<Guid> GetSelectionListAsGuid() => ListSelection
+		public List<Guid> GetSelectionListAsGuid() => ListSelection?
 			.Select(x => Guid.TryParse(x, out Guid result) ? (Guid?)result : null)
 			.Where(x => x != null).Select(x => (Guid)x)
 			.ToList();
