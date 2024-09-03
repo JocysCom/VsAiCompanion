735afe4a25c4a10157cb13c5b1f31b105bf1ef6f
Evaldas Jocys <evaldas@jocys.com>
2024-09-03 07:06:47 +0100

    add function class

---
+++ Engine/Companions/ChatGPT/chat_completion_function_parameter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace JocysCom.VS.AiCompanion.Engine.Companions.ChatGPT
+{
+	public class chat_completion_function_parameter : base_item
+	{
+		[DefaultValue(null)] public string type { get; set; }
+		[DefaultValue(null)] public Dictionary<string, object> properties { get; set; }
+		[DefaultValue(null)] public List<string> required { get; set; }
+
+	}
+}
