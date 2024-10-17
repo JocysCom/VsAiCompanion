adc654b891c8aad0d3645d712cd77f58cbffd376
Evaldas Jocys <evaldas@jocys.com>
2024-10-17 07:27:46 +0100

    New: Ask AI Window.

---
+++ Engine/Controls/Shared/AiWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace JocysCom.VS.AiCompanion.Engine.Controls.Shared
+{
+	/// <summary>
+	/// Interaction logic for AI Window.
+	/// </summary>
+	public partial class AiWindow : Window
+	{
+		public AiWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void SendButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void DataTextBox_PreviewKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+
+		}
+
+		private void DataTextBox_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+		{
+
+		}
+
+		private void DataTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+		{
+
+		}
+	}
+}
