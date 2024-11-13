0b48bb005bcc5f9a1798bf82bd3c791c33641a33
Evaldas Jocys <evaldas@jocys.com>
2024-11-13 11:14:58 +0000

    Update package management.

--- Clients/OpenAI/Models/model.cs
+++ Clients/OpenAI/Models/model.cs
@@ -1,11 +1,10 @@
-using System.Collections.Generic;
 namespace JocysCom.VS.AiCompanion.Clients.OpenAI.Models
 {
-    public class model : delete_assistant_file_response
-    {
-        public int created { get; set; }
+	public class @model : delete_assistant_file_response
+	{
+		public int created { get; set; }
 
-        public string owned_by { get; set; }
+		public string owned_by { get; set; }
 
-    }
+	}
 }
