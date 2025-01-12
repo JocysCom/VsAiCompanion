dcfd32499c0319327ed2bb142740e7d213903a24
Evaldas Jocys <evaldas@jocys.com>
2025-01-12 15:20:26 +0000

    Update version to 1.16.18

--- Data/DataFunctions/Properties/AssemblyInfo.cs
+++ Data/DataFunctions/Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Reflection;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.16.17")]
-[assembly: AssemblyFileVersion("1.16.17")]
+[assembly: AssemblyVersion("1.16.18")]
+[assembly: AssemblyFileVersion("1.16.18")]
