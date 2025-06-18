3b07ea9705edaf5762a68c1ddc675ab4434e2305
Evaldas Jocys <evaldas@jocys.com>
2025-06-18 14:53:36 +0100

    Update API

--- Tools/Docker/Setup_4b_Firecrawl_API.ps1
+++ Tools/Docker/Setup_4b_Firecrawl_API.ps1
@@ -210,6 +210,8 @@ function Install-FirecrawlContainer {
 		"--env", "REDIS_HOST=$global:redisNetworkAlias",
 		"--env", "REDIS_PORT=$global:redisPort",
 		"--env", "PLAYWRIGHT_MICROSERVICE_URL=http://playwright-service:3000/scrape", # Required for web scraping
+		"--env", "HOST=0.0.0.0", # Bind to all interfaces
+		"--env", "PORT=$global:firecrawlPort", # Set the port
 		"--env", "POSTHOG_API_KEY=" # Disable PostHog analytics.
 	)
 
